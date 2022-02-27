d32187b31153b5751aa360c72e6441e529406a7e
jkehelwala <git@jkehelwala.com>
2022-02-28 02:15:01 +0530

    script edit

--- conf/exp_scalable.sh
+++ conf/exp_scalable.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # nohup sh conf/exp_scalable.sh > scalable.log &
 
-../NetLogo/netlogo-headless.sh --model autarki.nlogo --setup-file conf/exp_scalable.xml --experiment 1.4.no_of_peers
-echo "Completed: 1.4.no_of_peers"
+date
+../NetLogo/netlogo-headless.sh --model autarki.nlogo --setup-file conf/exp_scalable.xml --experiment 5.1.tolerance_scalable
+echo "Completed: 5.1.tolerance_scalable"
+date
