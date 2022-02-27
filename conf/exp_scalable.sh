2a76a30a34a1769c95494815266c9af38653ed21
jkehelwala <git@jkehelwala.com>
2022-02-27 11:52:28 +0530

    Scalability temporary_test

--- conf/exp_scalable.sh
+++ conf/exp_scalable.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# nohup sh conf/exp_peerno.sh > peerno.log &
+# nohup sh conf/exp_scalable.sh > scalable.log &
 
 ../NetLogo/netlogo-headless.sh --model autarki.nlogo --setup-file conf/exp_scalable.xml --experiment 1.4.no_of_peers
 echo "Completed: 1.4.no_of_peers"
