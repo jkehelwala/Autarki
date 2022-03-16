f36c8a88c0834862fcef1ffbcf2d7b50ae6614cd
jkehelwala <git@jkehelwala.com>
2022-03-16 20:33:40 +0530

    Scalable finalized

--- conf/exp_scalable.sh
+++ conf/exp_scalable.sh
@@ -2,6 +2,6 @@
 # nohup sh conf/exp_scalable.sh > scalable.log &
 
 date
-../NetLogo/netlogo-headless.sh --model autarki.nlogo --threads 1 --setup-file conf/exp_scalable.xml --experiment 5.1.tolerance_scalable
-echo "Completed: 5.1.tolerance_scalable"
+../NetLogo/netlogo-headless.sh --model autarki.nlogo --threads 1 --setup-file conf/exp_scalable.xml --experiment 5.1.scalable
+echo "Completed: 5.1.scalable"
 date
